421399f706addf439cedbfa921f891090b339563
Olav <73853649+olavorw@users.noreply.github.com>
2024-11-09 18:36:01 -0800

    Initial Commit

--- src/Main.java
+++ src/Main.java
@@ -12,4 +12,4 @@ public class Main {
             System.out.println("i = " + i);
         }
     }
-}
+} //TIP Press <shortcut actionId="ReformatCode"/> to reformat your code. This will make it easier to read and understand.
